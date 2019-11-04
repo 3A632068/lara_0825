cd6b673f29df1a45cc98a46fdcb97fe82a2219b1
PengHueiSin <eason12326@gmail.com>
2019-11-04 19:41:49 +0800

    設定 navigation.blade.php 上的 a 標籤 確認所有 href 設定都更改為對應的 route 名稱

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,7 +57,7 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-preview">
-                    <a href="post.blade.php">
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Man must explore, and this is exploration at its greatest
                         </h2>
@@ -69,7 +69,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.blade.php">
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
                         </h2>
@@ -78,7 +78,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.blade.php">
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Science has not yet mastered prophecy
                         </h2>
@@ -90,7 +90,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.blade.php">
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Failure is not an option
                         </h2>
