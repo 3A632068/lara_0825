0fc1630d76ccbe442d65493f120b8f5d5619f477
PengHueiSin <eason12326@gmail.com>
2019-12-10 21:59:21 +0800

    練習6-2-3.4:取得 Model

--- routes/web.php
+++ routes/web.php
@@ -68,8 +68,12 @@ Route::get('/', function () {
     dd($featuredPosts);*/
 
     //練習6-2-1.2
-    $fourthPost=\App\Post::find(4);
-    dd($fourthPost);
+    /*$fourthPost=\App\Post::find(4);
+    dd($fourthPost);*/
+
+    //練習6-2-3.4
+    $lastPost=\App\Post::orderBy('id','DESC')->first();
+    dd($lastPost);
 
     return view('welcome');
 });
